e8f5ac3cc623911412ffbb50f12d4cfc97eebbde
karlberry88 <karl.berry@sky.uk>
2024-04-15 11:50:12 +0100

    Update character.dart to change the name and slogan of a character

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -37,5 +37,5 @@ List<Character> characters = [
   Character(id: '1', name: 'Klara', vocation: Vocation.wizard, slogan: 'Kapumf!'),
   Character(id: '2', name: 'Jonny', vocation: Vocation.junkie, slogan: 'Light me up...'),
   Character(id: '3', name: 'Crimson', vocation: Vocation.raider, slogan: 'Fire in the hole!'),
-  Character(id: '4', name: 'Shaun', vocation: Vocation.ninja, slogan: 'Alright then gang.'),
+  Character(id: '4', name: 'Karl', vocation: Vocation.ninja, slogan: 'Will get the Flutter JOB!!.'),
 ];
