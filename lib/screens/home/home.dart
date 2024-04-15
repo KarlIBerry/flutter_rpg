b6946e7145da611f20006e856b2ff83c2a62ebda
karlberry88 <karl.berry@sky.uk>
2024-04-15 13:45:39 +0100

    Update home.dart to remove unused imports and improve code readability

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/screens/create/create.dart';
 import 'package:flutter_rpg/screens/home/character_card.dart';
 import 'package:flutter_rpg/services/character_store.dart';
 import 'package:flutter_rpg/shared/style_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:provider/provider.dart';
+
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -16,27 +16,27 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const StyledTitle('Your Characters'),
         centerTitle: true,
-        ),
+      ),
       body: Container(
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
+            // list of characters
             Expanded(
               child: Consumer<CharacterStore>(
-                builder: (context, value, child){
+                builder: (context, value, child) {
                   return ListView.builder(
-                  itemCount: value.characters.length,
-                  itemBuilder: (_, index) {
-                    return CharacterCard(characters[index]);
-                  },
-                );
+                    itemCount: value.characters.length,
+                    itemBuilder: (_, index) {
+                      return CharacterCard(value.characters[index]);
+                    }
+                  );
                 }
               ),
             ),
@@ -44,14 +44,14 @@ class _HomeState extends State<Home> {
             StyledButton(
               onPressed: () {
                 Navigator.push(context, MaterialPageRoute(
-                  builder: (ctx) => const CreateScreen()
+                  builder: (ctx) => const CreateScreen(),
                 ));
               },
               child: const StyledHeading('Create Character'),
-              ),
-          ],
-        )
-      )
+            ),
+          ]
+        ),
+      ),
     );
   }
 }
