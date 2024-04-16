3337e3614e9fc7390f286d8aebe0f972039c52fd
karlberry88 <karl.berry@sky.uk>
2024-04-16 13:46:19 +0100

    Refactor character.dart to add fromFirestore factory method

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,13 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/models/stats.dart';
 import 'package:flutter_rpg/models/vocation.dart';
 
 class Character with Stats {
-
   // constructor
   Character({
-    required this.name, required this.slogan, required this.vocation, required this.id,
+    required this.name,
+    required this.slogan,
+    required this.vocation,
+    required this.id,
   });
 
   // fields
@@ -38,17 +41,54 @@ class Character with Stats {
       'isFav': _isFav,
       'vocation': vocation.toString(),
       'skills': skills.map((s) => s.id).toList(),
-      'stats' : statsAsMap,
+      'stats': statsAsMap,
       'points': points,
     };
+  }
+
+// character from firestore
+  factory Character.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options) {
+
+// get data from snapshot
+    final data = snapshot.data();
+
+// make character instance
+    Character character = Character(
+      name: data!['name'],
+      slogan: data['slogan'],
+      id: snapshot.id,
+      vocation:
+          Vocation.values.firstWhere((v) => v.toString() == data['vocation'])
+    );
+
+    // update skills
+    for (String id in data ['skills']) {
+      Skill skill = allSkills.firstWhere((element) => element.id == id);
+      character.updateSkills(skill);
+    }
   }
 }
 
 // dummy character data
 
 List<Character> characters = [
-  Character(id: '1', name: 'Klara', vocation: Vocation.wizard, slogan: 'Kapumf!'),
-  Character(id: '2', name: 'Jonny', vocation: Vocation.junkie, slogan: 'Light me up...'),
-  Character(id: '3', name: 'Crimson', vocation: Vocation.raider, slogan: 'Fire in the hole!'),
-  Character(id: '4', name: 'Karl', vocation: Vocation.ninja, slogan: 'Will get the Flutter JO.'),
+  Character(
+      id: '1', name: 'Klara', vocation: Vocation.wizard, slogan: 'Kapumf!'),
+  Character(
+      id: '2',
+      name: 'Jonny',
+      vocation: Vocation.junkie,
+      slogan: 'Light me up...'),
+  Character(
+      id: '3',
+      name: 'Crimson',
+      vocation: Vocation.raider,
+      slogan: 'Fire in the hole!'),
+  Character(
+      id: '4',
+      name: 'Karl',
+      vocation: Vocation.ninja,
+      slogan: 'Will get the Flutter JO.'),
 ];
